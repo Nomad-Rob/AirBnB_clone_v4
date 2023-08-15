11f33ea651167ea619bbbef71c59e0bd414ec10d
Caaronbll <aaronbll46@gmail.com>
2023-08-14 22:44:05 -0700

    javascript for 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -39,6 +39,32 @@ $(function () {
       apiStatusDiv.removeClass('available');
     }
   });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      $('SECTION.places').append(data.map(place => {
+        return `<article>
+                  <div class="title_box">
+                    <h2>${place.name}</h2>
+                    <div class="price_by_night">${place.price_by_night}</div>
+                  </div>
+                  <div class="information">
+                    <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                  </div>
+                  <div class="description">
+                    ${place.description}
+                  </div>
+                </article>`
+      }));
+    }
+  });
 });
 
 
